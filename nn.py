e96b93dc912348f91a3ea78dfbd3c461a4d1af62
rvdmaazen <rvandermaazen@gmail.com>
2019-09-28 17:47:29 +0200

    Implement other activation functions

--- nn.py
+++ nn.py
@@ -1,8 +1,60 @@
 import numpy as np
 
 
+class Sigmoid:
+    def function(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def derivative(self, x):
+        return self.function(x) * (1 - self.function(x))
+
+
+class Tanh:
+    def function(self, x):
+        return np.tanh(x)
+
+    def derivative(self, x):
+        return 1 - np.tanh(x) ** 2
+
+
+class ReLU:
+    def function(self, x):
+        return np.maximum(0, x)
+
+    def derivative(self, x):
+        return np.where(x < 0, 0, 1)
+
+
+class ELU:
+    def __init__(self, alpha=0.01):
+        self.alpha = alpha
+
+    def function(x):
+        return np.where(x < 0, self.alpha * (np.exp(x) - 1), x)
+
+    def derivative(x):
+        return np.where(x < 0, self.function(x) + self.alpha, 1)
+
+
+class LeakyReLU:
+    def function(x):
+        return np.where(x < 0, 0.01 * x, x)
+
+    def derivative(x):
+        return np.where(x < 0, 0.01, 1)
+
+
+ACTIVATION_FUNCTIONS = {
+    "sigmoid": Sigmoid,
+    "tanh": Tanh,
+    "relu": ReLU,
+    "elu": ELU,
+    "leaky_relu": LeakyReLU,
+}
+
+
 class NeuralNetwork:
-    def __init__(self, layers, inputs, outputs, epochs, eta=0.01, batch_size=64):
+    def __init__(self, layers, inputs, outputs, epochs, activations, eta=0.01, batch_size=64):
         """
         Initializes the neural network using random weights and biases.
         Weights are intialized using Xavier initialization.
